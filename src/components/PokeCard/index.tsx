650b453dcee7485977ebbb00f0a21fc5dd4f9eff
Felipe de Oliveira Sousa <felipeoliveira1462@gmail.com>
2022-03-12 20:42:48 -0300

    Alternative image

--- src/components/PokeCard/index.tsx
+++ src/components/PokeCard/index.tsx
@@ -13,12 +13,14 @@ type CardDataProps = {
 
 export default function PokeCard({ data }: CardDataProps) {
     const [currentPokemon, setCurrentPokemon] = useState<PokemonProps>()
+    const [image, setImage] = useState<string>('')
 
     const router = useRouter()
 
     async function getPokemon() {
         await axios.get(data.url)
             .then(response => {
+                setImage(response.data.sprites.front_default)
                 setCurrentPokemon(response.data)
             })
             .catch((error: AxiosError) => {
@@ -34,6 +36,10 @@ export default function PokeCard({ data }: CardDataProps) {
         router.push(`/pokemon/${data.name}`)
     }
 
+    function changeAlternativeImage() {
+        setImage('/images/pikachu.png')
+    }
+
     return (
         <article className={styles.container} onClick={redirect}>
             {
@@ -41,7 +47,7 @@ export default function PokeCard({ data }: CardDataProps) {
                     <>
                         <h1 className={styles.title}>{data.name[0].toUpperCase() + data.name.substring(1)}</h1>
                         <div className={styles.image}>
-                            <Image src={currentPokemon.sprites.front_default} layout='fill' alt={currentPokemon.name} />
+                            <Image src={image} onError={changeAlternativeImage} layout='fill' alt={currentPokemon.name} />
                         </div>
                         <section>
                             <h2 className={styles.types}>Types</h2>
